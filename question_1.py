f86a77a2911924b6c54d0aa6b0894ca6e9cad1ba
Itzspencer235 <Itzspencer235@gmail.com>
2026-03-14 09:37:58 +0200

    first commit

---
+++ question_1.py
@@ -0,0 +1,5 @@
+cost=1200000
+deposit=300000
+remaining_balance=cost-deposit
+monthly_installment_amount=remaining_balance/6
+print("The remaining balance is "+ str(remaining_balance) +" rwf and should pay "+ str(monthly_installment_amount)+" rwf for 6 months")
